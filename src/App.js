d1f7183488a7ddc80a92fde8b9c6d65b78c18ce1
Deepak Rana <deepakplacement20@gmail.com>
2022-02-26 23:17:42 +0530

    admin listing all books

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import {Admin ,Resource} from 'react-admin';
-import restProvider  from 'ra-data-simple-rest';
+// import restProvider  from 'ra-data-simple-rest';
 import postList from './Components/postList';
 import {authProvider, dataProvider , firebaseRealtime} from './firebaseConfig.js';
+import  postCreate from ''
 // import notFound from './Components/notFound';
 function App() {
   return (
     <Admin   dataProvider={dataProvider} title='Myrsa cms'  >
      
-      <Resource name='books' list={postList} />
+      <Resource name='books' list={postList} create={postCreate}  />
 
      
     </Admin>
